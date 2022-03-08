7e4f84c3569cfc27503d46f41edb55f927aaf0d1
chdolf <chbabuunysis@gmail.com>
2022-03-08 16:14:52 +0530

    committing all files

---
+++ src/controllers/orders_controller.js
@@ -0,0 +1,19 @@
+const get = (req, res, next) => {
+  try {
+    const orders = [
+      { id: '1', productId: '1a', orderFor: 'Peter Parker' },
+      { id: '2', productId: '1b', orderFor: 'Steve Rogers' },
+      { id: '3', productId: '1c', orderFor: 'Tony Stark' },
+      { id: '4', productId: '1d', orderFor: 'Reed Richards' },
+    ];
+    res.status(200).send(orders);
+  } catch (e) {
+    next();
+  }
+};
+
+const OrdersController = {
+  get
+};
+
+module.exports = OrdersController;
